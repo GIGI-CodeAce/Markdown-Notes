f6bec33c6fae0c07a614a1057b9a2962482ae97e
Dobre Robert <dobrerobert441@gmail.com>
2025-01-21 17:09:28 +0200

    project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Routes,Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
+  <Routes>    
+    <Route path='/' element={<h1>Home</h1>}/>
+    <Route path='/new' element={<h1>New</h1>}/>
+    <Route path='/:id'>
+    <Route index element={<h1>Show</h1>}/>
+    <Route path='edit' element={<h1>Edit</h1>}/>
+    </Route>
+    <Route path='*' element={<Navigate to="/"/> }/>
+  </Routes>
   );
 }
 

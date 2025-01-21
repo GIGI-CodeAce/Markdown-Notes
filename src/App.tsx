9186ee69b79bf0a7ed34913a218ed725bd870778
Dobre Robert <dobrerobert441@gmail.com>
2025-01-21 14:06:50 +0200

    Save work before eject

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
       <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
       </header>
     </div>
   );

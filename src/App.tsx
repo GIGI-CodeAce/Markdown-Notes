c602ff2676a4d40867e5f19e9bf96225972978c8
Dobre Robert <dobrerobert441@gmail.com>
2025-01-26 12:41:04 +0200

    big changes, half the porject

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect, useMemo } from "react";
 import { Container } from 'react-bootstrap';
-import { NewNote } from "./NewNote";
+import { NewNote } from './NewNote';
+import { useLocalStorage } from './useLocalStorageHook';
+import {nanoid} from 'nanoid'
 
 export type Note = {
   id: string,
 } & NoteData
+
+export type rawNote ={
+  id: string,
+}& rawNoteData
+
+export type rawNoteData ={
+  title: string,
+  markdown: string,
+  tagIds: string[],
+}
 
 export type NoteData ={
   title: string,
@@ -19,11 +32,26 @@ export type Tag = {
 }
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<rawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote( {tags, ...data}: NoteData){
+    setNotes(prevNotes =>{
+      return [...prevNotes,{...data, id: nanoid(), tagIds: tags.map(tag => tag.id)}]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>    
         <Route path='/' element={<h1 className='m-2 underline'>Home</h1>}/>
-        <Route path='/new' element={<NewNote/>}/>
+        <Route path='/new' element={<NewNote onSubmit={onCreateNote}/>}/>
         <Route path='/:id'>
           <Route index element={<h1>Show</h1>}/>
           <Route path='edit' element={<h1>Edit</h1>}/>
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,65 +1,22 @@
-import { FormEvent, useRef, useState } from "react";
-import { Col, Form as BootstrapForm, Form, Row, Stack, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { NoteData, Tag } from "./App";
-import CreatableReactSelect from 'react-select/creatable';
 
-type NoteFormProps = {
-  onSubmit: (data: NoteData) => void;
-};
+import { NoteData, Tag } from "./App"
+import { NoteForm } from "./noteForm"
 
-export function NewNote({ onSubmit }: NoteFormProps) {
-  const titleref = useRef<HTMLInputElement>(null);
-  const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+}
 
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-
-    onSubmit({
-      title: titleref.current!.value,
-      markdown: markdownRef.current!.value,
-      tags: selectedTags,
-    });
-  }
-
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
-    <Form onSubmit={handleSubmit}>
-      <Stack gap={4}>
-        <Row>
-          <Col>
-            <BootstrapForm.Group controlId="title">
-              <BootstrapForm.Label>Title</BootstrapForm.Label>
-              <BootstrapForm.Control ref={titleref} required placeholder="Enter note title" />
-            </BootstrapForm.Group>
-          </Col>
-          <Col>
-            <BootstrapForm.Group controlId="tags">
-              <BootstrapForm.Label>Tags</BootstrapForm.Label>
-              <CreatableReactSelect
-                value={selectedTags.map(tag => {
-                  return { label: tag.label, value: tag.id };
-                })}
-                onChange={tags => setSelectedTags(tags.map(tag => {
-                  return { label: tag.label, id: tag.value };
-                }))}
-                isMulti
-                placeholder="Add tags..."
-              />
-            </BootstrapForm.Group>
-          </Col>
-        </Row>
-        <Col>
-          <BootstrapForm.Group controlId="markDown">
-            <BootstrapForm.Label>Body</BootstrapForm.Label>
-            <BootstrapForm.Control required as="textarea" ref={markdownRef} rows={15} placeholder="Write your note here..." />
-          </BootstrapForm.Group>
-          <Stack direction="horizontal" gap={3} className="justify-content-end">
-            <Button type="submit" variant="primary">Save</Button>
-            <Link to=".."><Button type="button" variant="outline-secondary">Cancel</Button></Link>
-          </Stack>
-        </Col>
-      </Stack>
-    </Form>
-  );
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  )
 }
